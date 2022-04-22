07a2517075e13ac56c53a14302ec4a48b83e9fae
Baran Ozer <ugurbaranozer@gmail.com>
2022-04-22 15:49:59 +0300

    navbar adjusted

--- web/src/components/pages/Navbar.js
+++ web/src/components/pages/Navbar.js
@@ -1,27 +1,17 @@
 import React from "react";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav} from "react-bootstrap";
 
 export default function NavbarTop() {
   return (
-    <Navbar className="justify-content-center">
+    <Navbar className="color-nav" variant="none">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav>
+            <Nav.Link href="#Information">Information</Nav.Link>
+            <Nav.Link href="#Teams">Teams</Nav.Link>
+            <Nav.Link href="#Scoreboard">Scoreboard</Nav.Link>
+            <Nav.Link href="#Challanges">Challanges</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
