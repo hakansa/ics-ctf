80716cdfa67b8b233a1092f7507a053dd239815a
Hakan SARIMAN <hknsrmn46@gmail.com>
2022-06-05 12:50:39 +0300

    add navbar sample

--- web/src/components/pages/NavbarPostLogin.js
+++ web/src/components/pages/NavbarPostLogin.js
@@ -1,6 +1,40 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 
+// write admin panel navbar by checking local storage username is admin
+function NavbarTop() {
+  if (localStorage.getItem("username") === "admin") {
+    return (
+      <Navbar className="color-nav" variant="none">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="#Admin">Admin Panel</Nav.Link>
+              <Nav.Link href="#Scoreboard">Scoreboard</Nav.Link>
+              <Nav.Link href="#Challenges">Challenges</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  } else {
+    return (
+      <Navbar className="color-nav" variant="none">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="#Scoreboard">Scoreboard</Nav.Link>
+              <Nav.Link href="#Challenges">Challenges</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
 export default function NavbarPostLog() {
   return (
     <Navbar className="nav-post" variant="bg">
@@ -19,7 +53,6 @@ export default function NavbarPostLog() {
       <Nav.Link className="text-left" href="/Challenges">
         Challenges
       </Nav.Link>
-
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" />
